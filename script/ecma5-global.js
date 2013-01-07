f4b7fd858ca59651b8ff7ff0642905a74c71c1af
toshif <toshif@toshif.com>
2013-01-08 01:02:47 +0900

    Global Objects

---
+++ script/ecma5-global.js
@@ -0,0 +1,16 @@
+/*
+ * ECMA-262 (ECMAScript) Global Objects
+ * 
+ * Mozilla - Global Objects 
+ * https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects
+ * 
+ * ECMAScript 5 specification
+ * http://www.ecma-international.org/publications/files/ECMA-ST/Ecma-262.pdf
+ */
+
+function Global() {
+}
+
+Global.Math = new Math();
+Global.JSON = new JSON();
+Global.Date = new Date();
